2ba5c637069274d9fead3ea8d777a621e7419721
Pierre-Louis NGO <pierre-louis.ngo@outlook.fr>
2021-12-06 14:46:18 +0100

    Rajout Widget Frontend

--- frontend/src/Components/Widget5.js
+++ frontend/src/Components/Widget5.js
@@ -1,11 +1,16 @@
 import React, { useState }  from 'react'
 
-export const AddStaff= ()=> {
+export const Widget5= ()=> {
 
     var [name,setName]=useState()
+    var [description,setDescription]=useState()
     
     const nameUpdate=(event)=>{ // Dealing with name field changes to update our state
         setName(event.target.value)
+    }
+
+    const descriptionUpdate=(event)=>{
+        setDescription(event.target.value)
     }
 
     const handleSubmit=()=> { // Once the form has been submitted, this function will post to the backend
@@ -18,7 +23,7 @@ export const AddStaff= ()=> {
             },
             body: JSON.stringify({ // We should keep the fields consistent for managing this data later
                 title: name,
-                description:"test du post"
+                description:description
             })
         })
         .then(()=>{
@@ -28,18 +33,26 @@ export const AddStaff= ()=> {
     }
 
     return (
-        <div className="card" >
-            <div className="card-body">
-            <h5>Add data </h5>
-            <form onSubmit={handleSubmit}>
-                <label>Enter a title:</label>
-                <input required onChange={nameUpdate}></input>
-                <button type="submit"> Submit</button>
-            </form>
-            </div>
-        </div>
+        <div className="card">
+       <div className="card-body">
+       <h5>Add data &#10133; </h5> 
+        <form onSubmit={handleSubmit}>
+  <div class="form-group">
+    <label for="example1">Title</label>
+    <input required onChange={nameUpdate} type="text" class="form-control" id="text1" placeholder="Enter title"/>
+  </div>
+  <div class="form-group">
+    <label for="example2">Description</label>
+    <input required onChange={descriptionUpdate} type="text" class="form-control" id="text2" placeholder="Enter description"/>
+  </div>
+  <br/>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+       
     )
     
 }
 
-export default AddStaff
+export default Widget5;
